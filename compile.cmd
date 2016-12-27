8e405d3996c717bc965897f8c8ba8844a4570935
Raymel Francisco <franciscoraymel@gmail.com>
2016-12-27 16:12:07 +0800

    Build file

--- compile.cmd
+++ compile.cmd
@@ -2,7 +2,7 @@
 :: compile.cmd
 :: Compiles jco.cpp to exe file
 ::
-:: (c) August 2015 Raymel Francisco
+:: (c) August 2016 Raymel Francisco
 ::
 @windres resource.rc -O coff -o resource.res
 @g++ -m32 -o jco.exe jco.cpp resource.res
